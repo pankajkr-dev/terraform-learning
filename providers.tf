51df276b15690a76bb26e473ee088d8c3b0eac95
Pankaj Kumar <pankajkr.vitj@gmail.com>
2026-08-19 17:59:28 +0530

    Update S3 remote backend bucket name

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
   # REMOTE BACKEND CONFIGURATION
   # ==========================================
   backend "s3" {
-    bucket         = "dev-terraform-state-pankaj-2026"
+    bucket         = "dev-terraform-state-pankaj-2026-v2"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "dev-terraform-state-locks"
